0d3c19bb00476a6e87f7fdd6503318d71eded0d0
Wanderson Veiga <wanderalez@gmail.com>
2021-03-19 20:08:28 +0000

    fix validator

--- packages/app/client/src/utils/validators.js
+++ packages/app/client/src/utils/validators.js
@@ -1,11 +1,11 @@
-export const validateEmail = (email) => {
+export const validateEmail = ({ email }) => {
   if (email) {
     if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
-      return 'Invalid value for email'
+      return { email: 'Invalid value for email' }
     }
 
-    return undefined
+    return {}
   } else {
-    return 'Email is required'
+    return { email: 'Email is required' }
   }
 }
