5c9c6052756a78ec7d52b34a9c53d175ee1aac14
Wanderson Veiga <wanderalez@gmail.com>
2021-03-19 09:39:50 +0000

    create simple form component

---
+++ packages/app/client/src/ui/organisms/SimpleFormWithHeader.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Form } from '../molecules'
+import { TitleAndSubtitle } from '../molecules'
+import Grid from '@material-ui/core/Grid'
+
+const SimpleFormWithHeader = (props) => {
+  return (
+    <Grid item xs={12}>
+      <TitleAndSubtitle
+        subtitle={props.subtitle}
+        title={props.title}
+        subtitleAlign={props.subtitleAlign}
+        subtitleAlignMobile={props.subtitleAlignMobile}
+        titleAlign={props.titleAlign}
+        titleAlignMobile={props.titleAlignMobile}
+      />
+
+      <Form
+        fields={props.fields}
+        validate={props.validate}
+        submit={props.onSubmit}
+        submitLabel={props.submitLabel}
+        buttonLayout={props.buttonLayout}
+      />
+    </Grid>
+  )
+}
+
+export default SimpleFormWithHeader
