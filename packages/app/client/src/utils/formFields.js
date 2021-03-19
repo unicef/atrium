ef0c0b65e2c47119c91f4de9314c81173ae461a9
Wanderson Veiga <wanderalez@gmail.com>
2021-03-19 09:43:11 +0000

    create form fields structure

---
+++ packages/app/client/src/utils/formFields.js
@@ -0,0 +1,30 @@
+module.exports = {
+  email: {
+    name: 'email',
+    id:'email',
+    label:'Your email',
+    initialValue: '',
+    htmlFor: 'emailInput'
+  },
+  name: {
+    name: 'name',
+    id:'name',
+    label:'Name',
+    initialValue: '',
+    htmlFor: 'nameInput'
+  },
+  surname: {
+    name: 'surname',
+    id:'surname',
+    label:'Surname',
+    initialValue: '',
+    htmlFor: 'surnameInput'
+  },
+  password: {
+    name: 'password',
+    id:'password',
+    label:'Password',
+    initialValue: '',
+    htmlFor: 'passwordInput'
+  },
+}
