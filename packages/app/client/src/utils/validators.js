28ec882199098a4403536c65c27b1d9de24afc1f
Wanderson Veiga <wanderalez@gmail.com>
2021-03-22 18:22:32 +0000

    create password validator

--- packages/app/client/src/utils/validators.js
+++ packages/app/client/src/utils/validators.js
@@ -1,11 +1,37 @@
-export const validateEmail = ({ email }) => {
+export const validateEmail = (email) => {
   if (email) {
     if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
       return { email: 'Invalid value for email' }
     }
 
-    return {}
-  } else {
-    return { email: 'Email is required' }
+    return undefined
   }
+
+  return { email: 'Email is required' }
 }
+
+export const validatePassword = (password) => {
+  if (password) {
+    const hasEightChars = password.length >= 8
+    const hasUpperCaseChar = /([A-Z])/g.test(password)
+    const hasNumberDigit = /\d/g.test(password)
+    
+    if (!hasEightChars) {
+      return { password: 'Use at least eight characters' }
+    }
+
+
+    if (!hasUpperCaseChar) {
+      return { password: 'Use at least one uppercase' }
+    }
+
+    if (!hasNumberDigit) {
+      return { password: 'Use at least one number digit' }
+    }
+
+    return {}
+  }
+
+  return { password: 'Password is required' }
+}
+
