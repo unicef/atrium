4761d6b32f67700ec19b7b50d6ffd976a39ba216
Wanderson Veiga <wanderalez@gmail.com>
2021-03-23 22:15:57 +0000

    fix positioning

--- packages/app/client/src/ui/atoms/PasswordInfoBubble.js
+++ packages/app/client/src/ui/atoms/PasswordInfoBubble.js
@@ -39,7 +39,7 @@ const useStyles = makeStyles(theme => ({
   },
   container: {
     position: 'absolute',
-    right: -300,
+    right: -290,
     top: 20,
     '@media (max-width: 1024px)': {
       display: 'flex',
