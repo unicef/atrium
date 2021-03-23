e957c74588d920bd9dc0435a4c9db38f0bdc7905
Wanderson Veiga <wanderalez@gmail.com>
2021-03-23 22:18:01 +0000

    create email sent screen

--- packages/app/client/src/ui/pages/Register/components/index.js
+++ packages/app/client/src/ui/pages/Register/components/index.js
@@ -1,2 +1,3 @@
 export { default as CreateAccountFooter } from './CreateAccountFooter'
 export { default as CreateAccountForm } from './CreateAccountForm'
+export { default as EmailSent } from './EmailSent'
