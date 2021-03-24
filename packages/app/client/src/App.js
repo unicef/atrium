77589ca5f89cd9cdf53f80e0d06ea2b7466eab51
Wanderson Veiga <wanderalez@gmail.com>
2021-03-24 10:22:12 +0000

    create new login page

--- packages/app/client/src/App.js
+++ packages/app/client/src/App.js
@@ -6,7 +6,7 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { logoutUser, setCurrentUser } from './actions/authActions'
-import Login from './components/auth/Login'
+//import Login from './components/auth/Login'
 import ForgotPassword from './components/auth/ForgotPassword'
 import ResetPassword from './components/auth/ResetPassword'
 import Dashboard from './components/dashboard/Dashboard'
@@ -43,7 +43,7 @@ import {
 import setAuthToken from './utils/setAuthToken'
 
 // pages
-import { Learn, Register } from './ui/pages'
+import { Register, Login, Learn } from './ui/pages'
 
 require('./utils/configureRequests')
 
