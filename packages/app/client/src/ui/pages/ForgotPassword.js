7398bbff5cc030dc00c634192b6b1384d4c41751
Wanderson Veiga <wanderalez@gmail.com>
2021-03-26 13:38:49 +0000

    fix forgot password and reset password flows

--- packages/app/client/src/ui/pages/ForgotPassword.js
+++ packages/app/client/src/ui/pages/ForgotPassword.js
@@ -7,17 +7,11 @@ import { validateEmail } from '../../utils/validators'
 import { sendForgotPasswordEmail } from '../../api/users'
 import { EmailSent } from './Register/components'
 
-const formProps = [
-  {
-    title: "Forgot password",
-    titleProps: {
-      alignMobile: "left",
-      align: "center"
-    },
-    validate: ({ email }) => validateEmail(email),
-    submitLabel: 'Reset password',
-    fields: [email],
-    buttonLayout: { xs: 8, sm: 8 }
+const formProps = {
+  title: "Forgot password",
+  titleProps: {
+    alignMobile: "left",
+    align: "center"
   },
   validate: ({ email }) => validateEmail(email),
   submitLabel: 'Reset password',
@@ -44,21 +38,10 @@ const ForgotPassword = () => {
   const verifyEmail = async ({ email }) => {
     try {
       await sendForgotPasswordEmail({ email })
-      changeStep(1)
+      saveEmail(email)
+      showEmailWasSent(true)
     } catch(e) {
-      // TODO: add handler
-      console.log(e)
-    }
-  }
-  const showSuccess = step === 2
-
-  const saveNewPassword = async ({ password }) => {
-    try {
-      await changeUserPassword({ password })
-      changeStep(2)
-    } catch(e) {
-      // TODO: add handler
-      console.log(e)
+      showToast({ message: e, severity: 'danger' }) 
     }
   }
 
