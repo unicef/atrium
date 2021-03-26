a5c304aeb1885ecc5d18cd0033cff1eb8bbea867
Wanderson Veiga <wanderalez@gmail.com>
2021-03-26 12:19:54 +0000

    update introduction section

--- packages/app/client/src/ui/pages/Learn/sections/Introduction.js
+++ packages/app/client/src/ui/pages/Learn/sections/Introduction.js
@@ -3,22 +3,25 @@ import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
 import { LearnIntroductionSVG } from '../../../assets'
 import { TwoPartySection } from '../../../templates'
+import { TitleAndSubtitle } from '../../../molecules'
 
 const Introduction = () => {
   return (
-    <TwoPartySection>
-      <Grid item xs={12} md={8}>
-        <Typography variant="h2">
-          Learning resources
-        </Typography>
-        <Typography variant="subtitle1">
-        Are you curious to learn about different UN use cases? Have you ever wondered how your entity could apply blockchain? 
-        </Typography>
+    <TwoPartySection
+      partiesContainerProps={[{ alignItems: 'center' }]}
+    >
+      <Grid item xs={12} lg={8}>
+        <TitleAndSubtitle
+          title="Learning resources"
+          subtitle="Are you curious to learn about different UN use cases? Have you ever wondered how your entity could apply blockchain?"
+          titleProps={{ align: 'left', alignMobbile: 'left' }}
+          subtitleProps={{ align: 'left', alignMobbile: 'left' }}
+        />
        <Typography variant="subtitle2">
-       Welcome to the Learn section of The Atrium. We have curated a list of the best blockchain resources for you. You'll find a mix of UN specific resources and public content.
+        Welcome to the Learn section of The Atrium. We have curated a list of the best blockchain resources for you. You'll find a mix of UN specific resources and public content.
        </Typography>
       </Grid>
-      <Grid item container xs={12} md={8}>
+      <Grid item container xs={12} lg={8}>
         <LearnIntroductionSVG />
       </Grid>
     </TwoPartySection>
