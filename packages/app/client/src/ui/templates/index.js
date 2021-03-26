93585732a5bed1f19114e5aa2defddf312d45833
Wanderson Veiga <wanderalez@gmail.com>
2021-03-26 00:58:03 +0000

    export templates

--- packages/app/client/src/ui/templates/index.js
+++ packages/app/client/src/ui/templates/index.js
@@ -1,3 +1,5 @@
 export { default as FilterableListTemplate } from './FilterableListTemplate'
 export { default as TitledModal } from './TitledModal'
 export { default as StandardVerticalTemplate } from './StandardVerticalTemplate'
+export { default as SectionWithBorderedText } from './SectionWithBorderedText'
+export { default as TwoPartySection } from './TwoPartySection'
