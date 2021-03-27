a59a7b6b5e81cc30e035efd4a662f575959296a7
Wanderson Veiga <wanderalez@gmail.com>
2021-03-27 01:42:04 +0000

    roll back error from the back-end

--- packages/app/client/src/utils/errorHandling.js
+++ packages/app/client/src/utils/errorHandling.js
@@ -1,6 +1,10 @@
 const errorHandling = (errorsObj) => (error, overwritingErrors = {}) => {
   const genericError = 'Oops, something went wrong... Please try again and if the issue persists email blockchain@uninnovation.network'
 
+  if (error.response?.data.err) {
+    return error.response.data.err
+  }
+
   if (error.response?.status) {
     const mergedErros = { ...errorsObj, ...overwritingErrors }
     const mappedError = mergedErros[error.response.status]
