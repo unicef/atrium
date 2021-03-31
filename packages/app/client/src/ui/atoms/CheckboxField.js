1ca20a0b1b0c0a0622b8adff4e7c7ef5c1bb32be
Wanderson Veiga <wanderalez@gmail.com>
2021-03-31 03:34:13 +0100

    update checkbox

--- packages/app/client/src/ui/atoms/CheckboxField.js
+++ packages/app/client/src/ui/atoms/CheckboxField.js
@@ -8,12 +8,23 @@ const useStyles = makeStyles(theme => ({
     color: theme.colors['light-gray'],
     '&$checked': {
       color: theme.palette.primary.main,
+      backgroundColor: 'transparent',
+      '&:hover': {
+        backgroundColor: 'transparent'
+      }
     },
+    '&:hover': {
+      backgroundColor: 'transparent'
+    },
+    '& svg': {
+      width: 26,
+      height: 26
+    }
   },
   checked: {},
 }))
 
-const CustomCheckbox = ({ fullWidth, formLabelProps, hasError, value, label, classes, ...props }) => {
+const CustomCheckbox = ({ contentPlacemet, fullWidth, formLabelProps, hasError, value, label, classes, ...props }) => {
   const styles = useStyles()
 
   const mergeClassesObj = Object.keys(styles).reduce((acc, key) => {
@@ -22,10 +33,11 @@ const CustomCheckbox = ({ fullWidth, formLabelProps, hasError, value, label, cla
     }
     return acc
   }, styles)
-  
+
   return (
     <FormControlLabel
       {...formLabelProps}
+      contentPlacemet={contentPlacemet}
       control={
         <Checkbox
           checked={value}
