902fa3c6f7f71ba250186314fc7fb0a0dd0ccbbe
Wanderson Veiga <wanderalez@gmail.com>
2021-03-31 03:29:21 +0100

    update template

--- packages/app/client/src/ui/templates/SectionWithBorderedText.js
+++ packages/app/client/src/ui/templates/SectionWithBorderedText.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
-import TwoPartySection from './TwoPartySection' 
+import SectionContainer from './SectionContainer' 
 import { BorderedTextBoxWithButton } from '../molecules'
-import { makeStyles } from '@material-ui/core/styles'
-import { useIsMobileViewPort } from '../hooks'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(theme => ({
   title: {
@@ -17,28 +17,46 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
-const SectionWithBorderedText = ({ reverse, boxTitle, boxDescription, actionLabel, bgColor, otherComponent, onClick, hideBorder }) => {
+const SectionWithBorderedText = ({
+  boxTitle,
+  boxDescription,
+  actionLabel,
+  bgColor,
+  otherComponent,
+  onClick,
+  hideBorder,
+  borderedTextFirst,
+  id
+}) => {
   const classes = useStyles()
-  const isMobileView = useIsMobileViewPort()
+  const theme = useTheme()
+  const matches = useMediaQuery(theme.breakpoints.down('sm'))
 
+  const isPrimary = !borderedTextFirst || matches
+  const isSecondary = borderedTextFirst && !matches
+
+  const wrappedComponent = (
+    <Grid item container xs={12} sm={10} md={5} lg={5}>
+      {otherComponent}
+    </Grid>
+  )
+ 
   return (
-    <TwoPartySection
-      bgColor={bgColor}
-      reverse={reverse && !isMobileView}
-    >
-      <Grid item container justify="center" xs={12} lg={8}>
-        {otherComponent}
-      </Grid>
-      <Grid direction="column" justify="center" container item xs={12} lg={8}>
-        <Typography className={classes.title} variant="h3">
-          {boxTitle}
-        </Typography>
+    <SectionContainer id={id} bgColor={bgColor} justify={matches ? 'center' : "space-between" } alignItems="center">
+      {isPrimary && wrappedComponent}
+      <Grid item container xs={12} sm={10} md={5} lg={5}>
+        <div>
+          <Typography className={classes.title} variant="h3">
+            {boxTitle}
+          </Typography>
+        </div>
 
-        <BorderedTextBoxWithButton gridProps={{ xs: 'auto' }} hideBorder={hideBorder} onClick={onClick} buttonLabel={actionLabel}>
+        <BorderedTextBoxWithButton hideBorder={hideBorder} onClick={onClick} buttonLabel={actionLabel}>
           {boxDescription}
         </BorderedTextBoxWithButton>
       </Grid>
-    </TwoPartySection>
+      {isSecondary && wrappedComponent}
+    </SectionContainer>
   )
 }
 
