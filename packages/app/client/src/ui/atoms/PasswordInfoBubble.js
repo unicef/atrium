e6bedb2818274ac8fca6bc4398635c7316962503
Wanderson Veiga <wanderalez@gmail.com>
2021-03-31 03:33:17 +0100

    update component

--- packages/app/client/src/ui/atoms/PasswordInfoBubble.js
+++ packages/app/client/src/ui/atoms/PasswordInfoBubble.js
@@ -3,49 +3,19 @@ import Grow from '@material-ui/core/Grow'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import CheckIcon from '@material-ui/icons/Check'
+import ChatBubbleShape from'./ChatBubbleShape'
 import { makeStyles } from '@material-ui/styles'
 
 const useStyles = makeStyles(theme => ({
-  talkbubble: {
-    width: 273,
-    height: 125,
-    padding: '15px 25px 15px 25px',
-    background: theme.colors['blue-info'],
-    position: 'relative',
-    borderRadius: '5px',
-    '@media (max-width: 1024px)': {
-      right: 0,
-      left: 0
-    }
-  },
-  triangle: {
-    position: 'absolute',
-    left: -15,
-    top: 0,
-    width: 0,
-    height: 0,
-    borderTop: `30px solid ${theme.colors['blue-info']}`,
-    borderLeft: '30px solid transparent',
-    borderRight: '30px solid transparent',
-    '@media (max-width: 1024px)': {
-      left: '45%',
-      right: 0,
-      top: -15,
-      borderTop: 0,
-      borderLeft: '20px solid transparent',
-      borderRight: '20px solid transparent',
-      borderBottom: `20px solid ${theme.colors['blue-info']}`
-    }
-  },
   container: {
     position: 'absolute',
-    right: -290,
-    top: 20,
-    '@media (max-width: 1024px)': {
+    right: 0,
+    top: 0,
+    [theme.breakpoints.down('md')]: {
       display: 'flex',
       left: 0,
       right: 0,
-      top: 100,
+      top: 0,
       justifyContent: 'center'
     }
   }
@@ -87,16 +57,15 @@ const PasswordInfoBubble = ({ isFocused }) => {
   return (
     <Grow in={isFocused} timeout={{ enter: 500 }}>
       <div className={classes.container}>
-        <div className={classes.talkbubble}>
-          <div className={classes.triangle} />
-          {rules.map((rule, index) => (
-            <RuleRow
-              key={`password_rule_${index}`}
-              className={classes.ruleRow}
-              label={rule}
-            />
-          ))}
-        </div>
+        <ChatBubbleShape height={125} top={20} left={30}>
+            {rules.map((rule, index) => (
+              <RuleRow
+                key={`password_rule_${index}`}
+                className={classes.ruleRow}
+                label={rule}
+              />
+            ))}
+        </ChatBubbleShape>
       </div>
     </Grow>
   )
