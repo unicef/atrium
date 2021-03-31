40aeb61f22554812a94e16bd949964b4776d5245
Wanderson Veiga <wanderalez@gmail.com>
2021-03-31 03:37:15 +0100

    update quiz section

--- packages/app/client/src/ui/pages/Learn/sections/Quiz.js
+++ packages/app/client/src/ui/pages/Learn/sections/Quiz.js
@@ -1,21 +1,32 @@
-import React from 'react'
-import { LearnQuizSVG } from '../../../assets'
+import React, { useState } from 'react'
+import Collapse from '@material-ui/core/Collapse'
 import { SectionWithBorderedText } from '../../../templates'
-import { QuizQuestions } from '../components'
+import { QuizQuestions, SectionIcon } from '../components'
 
 const Quiz = () => {
+  const [showQuiz, setQuizVisibility] = useState(false);
+
+  const showOrHideQuiz = () => {
+    setQuizVisibility((prev) => !prev);
+  }
+
   return (
     <>
       <SectionWithBorderedText
+        id="quizSection"
         boxDescription={`Before exploring whether blockchain is the right technology for you, it's important to have a clear understanding of the problem you are trying to solve and the goals you are trying to achieve. 
         Below is a set of questions meant to help determine whether blockchain could be beneficial for solving a specific problem and achieving desired outcomes.`}
         boxTitle="Is blockchain right for you?"
-        actionLabel="Start Quiz"
-        otherComponent={<LearnQuizSVG />}
+        actionLabel={showQuiz ? "" : "Start Quiz"}
+        otherComponent={<SectionIcon iconName="quiz" />}
         reverse
         hideBorder
+        onClick={showOrHideQuiz}
+        borderedTextFirst
       />
-      <QuizQuestions />
+      <Collapse in={showQuiz}>
+        <QuizQuestions  hideQuiz={showOrHideQuiz} />
+      </Collapse>
     </>
   )
 }
