3fbd1b56ca238feb00caf1df758933ba6225a32d
Wanderson Veiga <wanderalez@gmail.com>
2021-04-01 08:22:28 +0100

    fix step index

--- packages/app/client/src/ui/pages/Register/index.js
+++ packages/app/client/src/ui/pages/Register/index.js
@@ -35,7 +35,7 @@ const formProps = [
 ]
 
 function Register () {
-  const [step, changeStep] = useState(1)
+  const [step, changeStep] = useState(0)
   const [verifiedEmail, saveEmail] = useState(undefined)
   const containerStyle = useContainerStyle({ size: 'small' })
   const isMobileViewPort = useIsMobileViewPort()
