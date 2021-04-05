2c3230656e133ff51711e234bd94e4ea811a8359
Wanderson Veiga <wanderalez@gmail.com>
2021-04-05 15:24:11 +0100

    fix missing font

--- packages/app/client/src/ui/pages/Learn/sections/Introduction.js
+++ packages/app/client/src/ui/pages/Learn/sections/Introduction.js
@@ -4,8 +4,20 @@ import Grid from '@material-ui/core/Grid'
 import { SectionContainer } from '../../../templates'
 import { TitleAndSubtitle } from '../../../molecules'
 import { SectionIcon } from '../components'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  text: {
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '15px',
+    lineHeight: '180%',
+  }
+})
 
 const Introduction = () => {
+  const classes = useStyles()
+
   return (
     <SectionContainer justify="space-between" xs={12}>
       <Grid item xs={12} sm={10} md={5} lg={5}>
@@ -15,7 +27,7 @@ const Introduction = () => {
           titleProps={{ align: 'left', alignMobile: 'left' }}
           subtitleProps={{ align: 'left', alignMobile: 'left' }}
         />
-       <Typography variant="subtitle2">
+       <Typography variant="subtitle2" className={classes.text}>
           Welcome to the Learn section of The Atrium. We have curated a list of the best blockchain resources for you. You'll find a mix of UN specific resources and public content.
        </Typography>
       </Grid>
