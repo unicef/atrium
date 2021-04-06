e891e2ab71502160bbcce85366312344783f5869
Victor <71084861+victor11555@users.noreply.github.com>
2021-04-06 12:51:27 +0300

    Update updates.js

--- packages/app/lib/updates.js
+++ packages/app/lib/updates.js
@@ -1,10 +1,16 @@
 const Update = require('../models/Update')
+const log = require('../config/log')
 
 exports.createNewUpdate = async update => {
+  log.info(
+    {
+      update: update
+    },
+    'Creating new update'
+  )
   const newUpdate = new Update({
     ...update,
     date: Date.now()
   })
-  const savedUpdate = await newUpdate.save()
-  return savedUpdate
+  return await newUpdate.save()
 }
