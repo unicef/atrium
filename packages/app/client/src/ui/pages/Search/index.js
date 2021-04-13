e434fa2f4ec8f4775c65eef44201dde94a65dba8
Wanderson Veiga <wanderalez@gmail.com>
2021-04-13 17:06:59 +0100

    add button

--- packages/app/client/src/ui/pages/Search/index.js
+++ packages/app/client/src/ui/pages/Search/index.js
@@ -2,15 +2,22 @@ import React from 'react'
 import { SearchWithSideMenu } from '../../templates'
 import { SearchBar, SearchListWrapper } from './components'
 
-const Search = ({ Menu, List, title }) => {
+const Search = ({ Menu, List, title, searchBarRightSide }) => {
   // TODO: improve to use the query string in the browser url
   return (
     <SearchWithSideMenu
       SideMenuComponent={Menu}
-      SearchBarComponent={(props) => <SearchBar title={title} {...props} />} 
+      SearchBarComponent={(props) => <SearchBar rightSideComponent={searchBarRightSide} title={title} {...props} />} 
       ListComponent={() => <List WrapperComponent={SearchListWrapper} />}
     />
   )
+}
+
+Search.defaultProps = {
+  searchBarRightSide: null,
+  Menu: null,
+  List: null,
+  title: ''
 }
 
 export default Search
