6a7d368fda254b6a0b38618ae4dc752ff2f4c214
Victor <vzabrovskiu@gmail.com>
2021-04-26 15:36:45 +0300

    new way to send data, requests to BE remaining

--- packages/app/client/src/ui/pages/Account/index.js
+++ packages/app/client/src/ui/pages/Account/index.js
@@ -16,15 +16,12 @@ import {
   Notifications
 } from './components'
 import Grid from '@material-ui/core/Grid'
-import Panel from '../../../components/projects/overview/components/Panel'
 import { useSelector } from 'react-redux'
 
-const useStyles = makeStyles(() => ({}))
-
 const Account = () => {
-  const classes = useStyles()
   const containerStyle = useContainerStyle({ size: 'full' })
   const [tabIndex, setTabIndex] = useState(0)
+  const user = useSelector(state => state.auth.user)
 
   const handleChange = (e, newVal) => {
     setTabIndex(newVal)
@@ -34,15 +31,15 @@ const Account = () => {
       <Grid container xs={12}>
         <VerticalTabs handleChange={handleChange} tabIndex={tabIndex} />
         <InfoSection>
-          {tabIndex === 0 ? <Dashboard /> : null}
-          {tabIndex === 1 ? <Profile /> : null}
-          {tabIndex === 2 ? <Settings /> : null}
-          {tabIndex === 3 ? <MyPost /> : null}
-          {tabIndex === 4 ? <MyProjects /> : null}
-          {tabIndex === 5 ? <MyComments /> : null}
-          {tabIndex === 6 ? <Badges /> : null}
-          {tabIndex === 7 ? <Bookmarks /> : null}
-          {tabIndex === 8 ? <Notifications /> : null}
+          {tabIndex === 0 ? <Dashboard {...user} /> : null}
+          {tabIndex === 1 ? <Profile {...user} /> : null}
+          {tabIndex === 2 ? <Settings {...user} /> : null}
+          {tabIndex === 3 ? <MyPost {...user} /> : null}
+          {tabIndex === 4 ? <MyProjects {...user} /> : null}
+          {tabIndex === 5 ? <MyComments {...user} /> : null}
+          {tabIndex === 6 ? <Badges {...user} /> : null}
+          {tabIndex === 7 ? <Bookmarks {...user} /> : null}
+          {tabIndex === 8 ? <Notifications {...user} /> : null}
         </InfoSection>
       </Grid>
     </Container>
