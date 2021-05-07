36b6786ca804c9efc76e442686a63a733534b902
Wanderson Veiga <wanderalez@gmail.com>
2021-05-07 22:42:08 +0100

    create components

---
+++ packages/app/client/src/ui/atoms/Authorship.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(() => ({
+  author: {
+    fontStyle: 'normal',
+    fontWeight: 600,
+    fontSize: '14px',
+    lineHeight: '19.6px',
+  },
+  prefix: {
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '13px',
+    lineHeight: '18.2px',
+  }
+}))
+
+const Authorship = ({ author, prefix }) => {
+  const classes = useStyles()
+
+  return (
+    <>
+      {prefix && 
+        <Typography component="span" className={classes.prefix}>
+          Uploaded by
+        </Typography>
+      }
+      <Typography component="span" className={classes.author}>
+        {author}
+      </Typography>
+    </>
+  )
+}
+
+export default Authorship
