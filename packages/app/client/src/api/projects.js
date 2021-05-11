a4a0ef3d2362d70b9e4b6a82d22eac9301cc45e6
Wanderson Veiga <wanderalez@gmail.com>
2021-05-11 11:07:17 +0100

    update api requests

--- packages/app/client/src/api/projects.js
+++ packages/app/client/src/api/projects.js
@@ -40,10 +40,10 @@ export const toggleProjectLike = (projectId) => projectRequest({
  * @param {Comment} comment
  * @returns
  */
-export const addComment = (projectId, content) => projectRequest({
+export const addComment = (projectId, content, mentions) => projectRequest({
   method: 'post',
   endpoint: getCommentEndpoint(projectId),
-  body: { content }
+  body: { content, mentions }
 })
 
 /**
