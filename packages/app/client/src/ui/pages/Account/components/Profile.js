a4af31fcb710403b3e650c3269c6822306dcec87
Victor <vzabrovskiu@gmail.com>
2021-05-13 13:48:40 +0300

    form added

--- packages/app/client/src/ui/pages/Account/components/Profile.js
+++ packages/app/client/src/ui/pages/Account/components/Profile.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import { SimpleFormWithHeader } from '../../../organisms'
+import {
+  name,
+  bio,
+  role,
+  organization,
+  website
+} from '../../../../utils/formFields'
+import { MainContainer } from '../../../templates'
 
 function Profile(props) {
-    return (
-        <div>profile</div>
-    )
+  const fields = [
+    { ...name, initialValue: props.name || '' },
+    { ...bio, initialValue: props.bio || '' },
+    { ...role, initialValue: props.role || '' },
+    { ...organization, initialValue: props.organization || '' },
+    website
+  ]
+
+  return (
+    <MainContainer size="small" mt="-50px" margin="0">
+      <SimpleFormWithHeader
+        fields={fields}
+        title="Profile"
+        submitLabel="Save changes"
+        titleProps={{ align: 'left' }}
+        buttonLayout={{ xs: 4 }}
+      />
+    </MainContainer>
+  )
 }
 
 export default Profile
