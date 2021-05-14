84b34cf0b0972ca0c3a27a97300728c30399cc97
Victor <vzabrovskiu@gmail.com>
2021-05-14 14:40:36 +0300

    problem with request

--- packages/app/client/src/api/users.js
+++ packages/app/client/src/api/users.js
@@ -33,7 +33,7 @@ const usersRequest = baseRequest({ errors: ERRORS, baseURL: ROUTE })
 // REQUESTS
 export const updateUserDetails = userDetails =>
   usersRequest({
-    method: 'patch',
+    method: 'post',
     body: userDetails
   })
 
