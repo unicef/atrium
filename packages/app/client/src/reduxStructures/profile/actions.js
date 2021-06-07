f15fbec5dd2d7e5b57944ee9a027b1e9459dd2a3
Wanderson Veiga <wanderalez@gmail.com>
2021-06-07 13:04:23 +0100

    add flag to redux

--- packages/app/client/src/reduxStructures/profile/actions.js
+++ packages/app/client/src/reduxStructures/profile/actions.js
@@ -4,3 +4,4 @@ export const saveUserInformation = (payload) => ({ type: TYPES.SAVE_USER_INFO, p
 export const saveProfileProjects = (payload) => ({ type: TYPES.SAVE_PROJECTS, payload })
 export const likeProfileProject = (payload) => ({ type: TYPES.LIKE_PROJECT, payload })
 export const saveProfileActivities = (payload) => ({ type: TYPES.SAVE_ACTIVITIES, payload })
+export const setLoadMoreActivitiesFlag = (payload) => ({ type: TYPES.SET_LOAD_MORE_FLAG, payload })
