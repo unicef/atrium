720df40f054af75ae5faa4b5a3c3f5a54d8739ec
Wanderson Veiga <wanderalez@gmail.com>
2021-06-07 13:04:50 +0100

    update page

--- packages/app/client/src/ui/pages/Profile/components/about/Activities.js
+++ packages/app/client/src/ui/pages/Profile/components/about/Activities.js
@@ -4,19 +4,39 @@ import CircularProgress from '@material-ui/core/CircularProgress'
 import Activity from './Activity'
 import TabContentTitle from '../TabContentTitle'
 import { useSelector } from 'react-redux'
-import { getProfileUserActivities } from '../../../../../selectors'
+import { getProfileUserActivities, getLoadMoreActivitiesFlag } from '../../../../../selectors'
 import { Button } from '../../../../atoms'
 import { useProfileAsyncActions } from '../../../../hooks'
 import { EmptyResults } from '../../../../molecules'
 
 const Activities = () => {
   const activities = useSelector(getProfileUserActivities)
+  const loadMoreFlag = useSelector(getLoadMoreActivitiesFlag)
   const [isLoading, setLoadiing] = React.useState(false)
   const { getMoreActivities } = useProfileAsyncActions()
 
   const loadMore = () => {
     const offset = activities.length
     getMoreActivities(offset, setLoadiing)
+  }
+
+  const handleLoadButton = () => {
+    if (isLoading) {
+      return <CircularProgress />
+    }
+
+    if (loadMoreFlag === 'LOAD') {
+      return (
+        <Button
+          onClick={loadMore}
+          variant="outlined"
+        >
+          Load more
+        </Button>
+      )
+    }
+
+    return null
   }
 
   const handleActivitiesRender = () =>{
@@ -27,14 +47,7 @@ const Activities = () => {
             <Activity key={activity.id} {...activity} />
           ))}
           <Grid container item xs={12} justify={isLoading ? 'center' : 'flex-start'}>
-          {isLoading ?
-            <CircularProgress /> :
-            <Button
-              onClick={loadMore}
-              variant="outlined"
-            >
-              Load more
-            </Button>}
+          {handleLoadButton()}
           </Grid>
         </>
       )
