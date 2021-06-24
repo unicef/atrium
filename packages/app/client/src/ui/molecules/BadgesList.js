f0e07ffc05b764736552b8394a9d7e1eaa0c8182
Victor <vzabrovskiu@gmail.com>
2021-06-24 15:39:04 +0300

    add texts to badges page

--- packages/app/client/src/ui/molecules/BadgesList.js
+++ packages/app/client/src/ui/molecules/BadgesList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Badge from './Badge'
-import {GreenBadge1,
+import {
+  GreenBadge1,
   GreenBadge2,
   GreenBadge3,
   GreenBadge4,
@@ -15,29 +16,87 @@ import {GreenBadge1,
   GreyBadge6,
   GreyBadge7
 } from '../pages/Account/assets'
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
 
 const badges = [
-  { level: 1, value: 10, greyImg: GreyBadge1, greenImg: GreenBadge1, title: 'Seed' },
-  { level: 2, value: 25, greyImg: GreyBadge2, greenImg: GreenBadge2, title: 'Sprout' },
-  { level: 3, value: 50, greyImg: GreyBadge3, greenImg: GreenBadge3, title: 'Budding' },
-  { level: 4, value: 100, greyImg: GreyBadge4, greenImg: GreenBadge4, title: 'Flowering' },
-  { level: 5, value: 250, greyImg: GreyBadge5, greenImg: GreenBadge5, title: 'Bunch of Flowers' },
-  { level: 6, value: 500, greyImg: GreyBadge6, greenImg: GreenBadge6, title: 'Growing Tree' },
-  { level: 7, value: 1000, greyImg: GreyBadge7, greenImg: GreenBadge7, title: 'Forest' }
+  {
+    level: 1,
+    value: 10,
+    greyImg: GreyBadge1,
+    greenImg: GreenBadge1,
+    title: 'Seed'
+  },
+  {
+    level: 2,
+    value: 25,
+    greyImg: GreyBadge2,
+    greenImg: GreenBadge2,
+    title: 'Sprout'
+  },
+  {
+    level: 3,
+    value: 50,
+    greyImg: GreyBadge3,
+    greenImg: GreenBadge3,
+    title: 'Budding'
+  },
+  {
+    level: 4,
+    value: 100,
+    greyImg: GreyBadge4,
+    greenImg: GreenBadge4,
+    title: 'Flowering'
+  },
+  {
+    level: 5,
+    value: 250,
+    greyImg: GreyBadge5,
+    greenImg: GreenBadge5,
+    title: 'Bunch of Flowers'
+  },
+  {
+    level: 6,
+    value: 500,
+    greyImg: GreyBadge6,
+    greenImg: GreenBadge6,
+    title: 'Growing Tree'
+  },
+  {
+    level: 7,
+    value: 1000,
+    greyImg: GreyBadge7,
+    greenImg: GreenBadge7,
+    title: 'Forest'
+  }
 ]
 
 function BadgesList({ start, end, earned }) {
   return (
     <>
-      {badges.slice(start, end).map(badge => (
-        <Badge
-          badge={badge.value}
-          level={badge.level}
-          title={badge.title}
-          last={+badge.level === end}
-          img={earned ? badge.greenImg : badge.greyImg}
-        />
-      ))}
+      {start === end && earned ? (
+        <Grid item xs={12}>
+          <Typography variant="body1">You haven’t earned badges yet</Typography>
+        </Grid>
+      ) : start === end ? (
+        <Grid item xs={12}>
+          <Typography variant="body1">
+            There is no more badges to earn
+          </Typography>
+        </Grid>
+      ) : (
+        badges
+          .slice(start, end)
+          .map(badge => (
+            <Badge
+              badge={badge.value}
+              level={badge.level}
+              title={badge.title}
+              last={+badge.level === end}
+              img={earned ? badge.greenImg : badge.greyImg}
+            />
+          ))
+      )}
     </>
   )
 }
