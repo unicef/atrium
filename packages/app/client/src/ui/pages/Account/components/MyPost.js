e4ad49d7e7911fe2ad04c512a87fed06daa9b603
Victor <vzabrovskiu@gmail.com>
2021-08-02 14:50:37 +0300

    add link, change name

--- packages/app/client/src/ui/pages/Account/components/MyPost.js
+++ packages/app/client/src/ui/pages/Account/components/MyPost.js
@@ -1,11 +1,31 @@
 import React from 'react'
-
 import Typography from '@material-ui/core/Typography'
+import { Grid, Link } from '@material-ui/core'
+import { MainContainer } from '../../../templates'
+import {makeStyles} from "@material-ui/core/styles";
 
-function MyPost(props) {
+const useStyles = makeStyles(() => ({
+  title: {
+    fontWeight: 'bold',
+    marginBottom: '7%'
+  }
+}))
+
+function MyPost({ name }) {
+  const classes = useStyles()
   return (
-    <Typography variant="h3">My post</Typography>
+    <MainContainer size="small" mt="-50px" margin="0">
+      <Grid item xs={12}>
+        <Typography className={classes.title} variant="h4">
+          My posts
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Link href={`/forum/user/${name.split(' ')[0].toLowerCase()}/posts`}>
+          This link will send you to your posts on NodeBB forum
+        </Link>
+      </Grid>
+    </MainContainer>
   )
 }
-
 export default MyPost
