a934bc47e3c2a256711c1a7bfb85d51578e43cee
Wanderson Veiga <wanderalez@gmail.com>
2021-08-16 16:22:40 +0100

    remove old code

--- packages/app/client/src/components/projects/modals/index.js
+++
@@ -1,4 +0,0 @@
-export { withModals } from './withModals'
-export { default as PreCreateModal } from './PreCreateModal'
-export { default as CancelUploadModal } from './CancelUploadModal'
-export { default as SuccessfulUploadedModal } from './SuccessfulUploadedModal'
